18829eb9c51a0e787964bf3fd7952f3e1ad3c623
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2019-07-29 00:41:53 +0200

    Extract _buildListItem method in app_list page

--- lib/pages/app_list/app_list.dart
+++ lib/pages/app_list/app_list.dart
@@ -12,37 +12,39 @@ class AppListPage extends StatelessWidget {
         title: const Text('Apps'),
       ),
       body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: const <Widget>[
-                Icon(
-                  Icons.local_florist,
-                  size: 42,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 4, right: 4),
-                  child: Icon(
-                    Icons.android,
-                    color: Colors.grey,
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 4, right: 4),
-                    child: Text('App 1'),
-                  ),
-                ),
-                Icon(
-                  Icons.check_circle_outline,
-                  color: Colors.green,
-                ),
-                Icon(Icons.chevron_right)
-              ],
+        itemBuilder: _buildListItem,
+      ),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: const <Widget>[
+          Icon(
+            Icons.local_florist,
+            size: 42,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 4, right: 4),
+            child: Icon(
+              Icons.android,
+              color: Colors.grey,
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 4, right: 4),
+              child: Text('App 1'),
+            ),
+          ),
+          Icon(
+            Icons.check_circle_outline,
+            color: Colors.green,
+          ),
+          Icon(Icons.chevron_right)
+        ],
       ),
     );
   }
